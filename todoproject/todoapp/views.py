640f98d9ecebe895eafc03a877cec8a9e9ebcc91
IUTA <vasavaseckin801@gmail.com>
2022-10-14 13:20:28 +0300

    adding new elements to the list

--- todoproject/todoapp/views.py
+++ todoproject/todoapp/views.py
@@ -3,5 +3,13 @@ from django.http import HttpResponseRedirect
 from .models import TodoListItem
 
 def todoappView(request):
-    all_todo_items = TodoListItem.object.all()
-    return render(request, 'todolist.html', {})
+    all_todo_items = TodoListItem.objects.all()
+    return render(request, 'todolist.html',
+    {'all_items':all_todo_items})
+
+# обработка запроса на публикацию
+def addTodoView(request): 
+    x = request.POST['content'] 
+    new_item = TodoListItem(content = x) 
+    new_item.save() 
+    return HttpResponseRedirect('/todoapp/')
--- todoproject/todoproject/urls.py
+++ todoproject/todoproject/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from todoapp.views import todoappView
+from todoapp.views import todoappView, addTodoView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todoapp/', todoappView),
+    path('addTodoItem/',addTodoView), 
+
 ] 
